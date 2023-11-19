4b15c6423d3c328d2e56654424b6b39547070f3b
Rodrigo <62319381+xrrodrigo@users.noreply.github.com>
2023-11-19 01:05:27 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const avatarImage = document.querySelector('#arquivo');
+const h2Avatar = document.querySelector('#h2-avatar');
+
+avatarImage.addEventListener('change', event => {
+  
+  const reader = new FileReader;
+  reader.onload = function(event) {
+    
+    const previewImage = document.createElement('img');
+  previewImage.id = 'preview-image';
+  previewImage.src = event.target.result;
+  h2Avatar.insertAdjacentElement('afterend', previewImage);
+  }
+  reader.readAsDataURL(avatarImage.files[0]);
+  
+  
+})
