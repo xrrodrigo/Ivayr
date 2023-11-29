83dc2c755b603598c24ae66b4e52027c6a1acb52
Rodrigo <62319381+xrrodrigo@users.noreply.github.com>
2023-11-28 23:24:25 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-function download() {
-  const canvas = document.getElementById("canvas");
-  const imgData = canvas.toDataURL("image/png");
-  const link = document.createElement("a");
-  link.href = imgData;
-  link.download = "minha-imagem.png";
-  link.click();
+function download(event) {
+  event.preventDefault();
 
+  canvas.toBlob(
+      blob => {
+          const anchor = window.document.createElement('a');
+          anchor.href = window.URL.createObjectURL(blob);
+          anchor.download = 'ivayr.jpg';
+          anchor.click();
+          window.URL.revokeObjectURL(anchor.href);
+
+      },
+      'image/jpeg',
+      0.9,
+  );
+  location.reload()
 }
