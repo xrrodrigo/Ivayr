4a5e3fcdd09bffc3180498e5acb8f58fd2658394
Rodrigo <62319381+xrrodrigo@users.noreply.github.com>
2023-12-21 04:23:02 -0300

    Update canva.js

--- canva.js
+++ canva.js
@@ -11,7 +11,7 @@ function drawnImage() {
 
     img1.onload = null; // Remove the onload handler to avoid memory leaks
 
-    img1.src = '/img/canvaimage.png'; // Reuse the img1 instance for the second image
+    img1.src = '/canvaimage.png'; // Reuse the img1 instance for the second image
 
     img1.onload = function() {
       context.drawImage(img1, 0, 0, canvas.width, canvas.height);
