c38a47e906fa488f336615a2d928da7eb240b576
Rodrigo <62319381+xrrodrigo@users.noreply.github.com>
2023-12-21 04:19:09 -0300

    Update canva.js

--- canva.js
+++ canva.js
@@ -6,15 +6,19 @@ function drawnImage() {
   var img1 = new Image();
   img1.src = URL.createObjectURL(arquivo.files[0]);
 
-  var img2 = new Image();
-  img2.src = './canvaimage.png';
-
   img1.onload = function() {
     context.drawImage(img1, 0, 0, canvas.width, canvas.height);
-  };
 
-  img2.onload = function() {
-    context.drawImage(img2, 0, 0, canvas.width, canvas.height);
+    img1.onload = null; // Remove the onload handler to avoid memory leaks
+
+    img1.src = '/img/canvaimage.png'; // Reuse the img1 instance for the second image
+
+    img1.onload = function() {
+      context.drawImage(img1, 0, 0, canvas.width, canvas.height);
+    };
   };
 }
-drawnImage();
+
+window.onload = function() {
+  drawnImage();
+};
