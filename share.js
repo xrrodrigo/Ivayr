6d34afb0fb662465de17b64c9633bb825c2325a2
Rodrigo <62319381+xrrodrigo@users.noreply.github.com>
2024-01-29 03:39:23 -0300

    Add files via upload

---
+++ share.js
@@ -0,0 +1,11 @@
+function callShare() {
+    const data = {
+        title: 'Gere fotos personalizadas de apoio ao ivayr soalheiro',
+        text: 'Compartilhe sua foto personalizada para engajar',
+        url: 'https://xrrodrigo.github.io/Ivayr/'
+    }
+    
+    if(navigator.canShare && navigator.canShare(data)){
+        navigator.share(data);
+    }else{}
+}
